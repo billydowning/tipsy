5f41da0423a9fab9cc1ab96506e5977b22303549
Billy Downing <wldowning@gmail.com>
2021-01-28 12:30:23 -0700

    linked up IB with Controller

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        //Deselect all tip buttons via IBOutlets
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        //Make the button that triggered the IBAction selected.
+        sender.isSelected = true
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by William Downing on 1/28/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
